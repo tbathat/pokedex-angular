79e8d01a7918b236640675b3919fdee405ddd91b
tbathat <tabathapradier@gmail.com>
2023-04-21 18:33:54 -0300

    Create and manipulate API request

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-console.log("Sucesso!");
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+//Interface de interação com uma promise
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    console.log("Requisição concluída!");
+  });
