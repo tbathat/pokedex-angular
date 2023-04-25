8daea75b696d0e2cff9ed12343e068e167b3d17b
tbathat <tabathapradier@gmail.com>
2023-04-24 21:10:57 -0300

    Create load pagination mecanism

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,41 @@
 const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+
+let offset = 0;
+const limit = 5;
 
 function convertPokemonToLi(pokemon) {
   return `
-    <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+  <li class="pokemon ${pokemon.type}">
+          <span class="number">#${pokemon.number}</span>
+          <span class="name">${pokemon.name}</span>
 
-          <div class="detail">
-              <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}
-              </ol>
-              <img
-                src="${pokemon.photo}"
-                alt="${pokemon.name}"
-              />
-          </div>
-        </li>
-    `
+        <div class="detail">
+            <ol class="types">
+              ${pokemon.types
+                .map((type) => `<li class="type ${type}">${type}</li>`)
+                .join("")}
+            </ol>
+            <img
+              src="${pokemon.photo}"
+              alt="${pokemon.name}"
+            />
+        </div>
+      </li>
+  `;
 }
 
+function loadPokemonItens(offset, limit) {
+  //Interface de interação com uma promise
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join("");
+    pokemonList.innerHTML += newHtml;
+  });
+}
 
-//Interface de interação com uma promise
-pokeApi.getPokemons().then((pokemons = []) => {
-  const newHtml = pokemons.map(convertPokemonToLi).join("");
-  pokemonList.innerHTML = newHtml;
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
+  loadPokemonItens(offset, limit);
 });
